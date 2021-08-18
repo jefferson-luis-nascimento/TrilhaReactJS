b989bf8cc62171d27ab20de867450e98cda0c780
Jefferson Luís Nascimento <je.luis.nascimento@hotmail.com>
2021-08-18 08:34:05 -0300

    implemented issues

--- 02-primeiro-app-react/dtmoney/src/App.tsx
+++ 02-primeiro-app-react/dtmoney/src/App.tsx
@@ -1,13 +1,31 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { NewTransactionModal } from './components/NewTransactionModal';
 import { GlobalStyle } from "./styles/global";
 
+Modal.setAppElement('#root');
 
 export function App() {
+  const [isNewTransactioModalOpen, setIsNewTransactioModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactioModalOpen(true);
+  }
+
+  function handleCloseNewTransactioModal() {
+    setIsNewTransactioModalOpen(false);
+  }
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+      
+      <NewTransactionModal 
+        isOpen={isNewTransactioModalOpen} 
+        onRequestClose={handleCloseNewTransactioModal} 
+      />
       <GlobalStyle />
     </>
   );
--- 02-primeiro-app-react/dtmoney/src/components/TransactionTable/index.tsx
+++ 02-primeiro-app-react/dtmoney/src/components/TransactionTable/index.tsx
@@ -1,6 +1,13 @@
+import { useEffect } from "react";
+import { api } from "../../services/api";
 import { Container } from "./styles";
 
 export function TransactionTable() {
+  useEffect(() => {
+    api.get('/transactions')
+      .then(response => console.log(response.data));
+  }, []);
+
   return (
     <Container>
       <table>
@@ -21,21 +28,9 @@ export function TransactionTable() {
           </tr>
           <tr>
             <td>Aluguél</td>
-            <td className="withdraw">- R$1.100,00</td>
+            <td className="withdraw"> - R$1.100,00</td>
             <td>Casa</td>
             <td>05/07/2021</td>
-          </tr>
-          <tr>
-            <td>Desenvolvimento de Web site</td>
-            <td>R$12000,00</td>
-            <td>Desevolvimento</td>
-            <td>21/07/2021</td>
-          </tr>
-          <tr>
-            <td>Desenvolvimento de Web site</td>
-            <td>R$12000,00</td>
-            <td>Desevolvimento</td>
-            <td>21/07/2021</td>
           </tr>
         </tbody>
       </table>
