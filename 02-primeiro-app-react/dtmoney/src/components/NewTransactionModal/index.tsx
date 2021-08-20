d0d3a6179143b72bb1bb1a47e599d05edab541f5
Jefferson Luís Nascimento <je.luis.nascimento@hotmail.com>
2021-08-19 21:10:08 -0300

    implemented issues

--- 02-primeiro-app-react/dtmoney/src/components/NewTransactionModal/index.tsx
+++ 02-primeiro-app-react/dtmoney/src/components/NewTransactionModal/index.tsx
@@ -21,14 +21,21 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
   const [type, setType] = useState('deposit');
   const [category, setCategory] = useState('');
 
-  function handleCreateNewTransaction(event: FormEvent) {
+  async function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
-    createTransaction({
+    await createTransaction({
       title,
       amount,
       type,
       category,
     });
+
+    setTitle('');
+    setAmount(0);
+    setType('deposit');
+    setCategory('');
+
+    onRequestClose();
   }
 
   return (
